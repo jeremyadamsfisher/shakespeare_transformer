d6c7001e99fcb39d223bb5c2b84bc8bccd7a69ab
Jeremy Fisher <jeremyadamsfisher@gmail.com>
2023-04-02 20:55:20 -0700

    remove option to not use wandb

--- gpt/cli.py
+++ gpt/cli.py
@@ -1,21 +1,16 @@
 import typer
 
-from gpt.utils import run_with_wanbd
-
 app = typer.Typer(pretty_exceptions_enable=False)
 
 
 @app.command()
-def train(large: bool = False, use_wandb: bool = False):
+def train(large: bool = False):
     from gpt.config import gpt_large, gpt_small
     from gpt.model import Gpt
     from gpt.train import train as train_
 
     config = gpt_large if large else gpt_small
-
-    with run_with_wanbd(skip=use_wandb is False) as (m, pl_train_kwargs):
-        model = train_(Gpt(config), config, pl_train_kwargs)
-        m(model)  # log model, if wandb is enabled
+    train_(Gpt(config), config)
 
 
 if __name__ == "__main__":
--- gpt/train.py
+++ gpt/train.py
@@ -1,29 +1,43 @@
-import lightning.pytorch as pl
+import tempfile
 
+import lightning.pytorch as pl
+import torch
+from pytorch_lightning.loggers import WandbLogger
+
+import wandb
 from gpt.config import GptConfig
 from gpt.data import ShakespeareDataModule
 
 
 class LogGenerationPeriodically(pl.Callback):
-    def __init__(self, decoder, log_periodicity=1000):
+    def __init__(self, decoder, log_periodicity=1000, wandb_logger=None):
         self.log_periodicity = log_periodicity
         self.decoder = decoder
+        self.wandb_logger = wandb_logger
 
     def on_train_batch_start(self, _a, model, _b, batch_idx):
         if batch_idx % self.log_periodicity == 0:
             output = model.generate()
             output = self.decoder(output).replace("\n", " ")
-            self.log.log_text("trn/generation", output)
+            if self.wandb_logger:
+                columns = ["generation"]
+                data = [[output]]
+                self.wandb_logger.log_text("trn/generation", columns=columns, data=data)
 
 
-def train(model, config: GptConfig, log_periodicity=1000, pl_train_kwargs=None):
-    if pl_train_kwargs is None:
-        pl_train_kwargs = {}
-    dm = ShakespeareDataModule(config, workers=-1)
-    dm.setup()
-    log_cb = LogGenerationPeriodically(dm.decode, log_periodicity)
-    trainer = pl.Trainer(
-        max_epochs=config.n_epochs, callbacks=[log_cb], **pl_train_kwargs
-    )
-    trainer.fit(model, dm)
-    return model
+def train(model, config: GptConfig, log_periodicity=1000):
+    with wandb.init(project="gpt-shakespeare") as run:
+        dm = ShakespeareDataModule(config, workers=-1)
+        dm.setup()
+        logger = WandbLogger()
+        log_cb = LogGenerationPeriodically(dm.decode, log_periodicity, logger)
+        trainer = pl.Trainer(
+            max_epochs=config.n_epochs, callbacks=[log_cb], logger=[logger]
+        )
+        trainer.fit(model, dm)
+        with tempfile.NamedTemporaryFile(suffix=".ckpt") as f:
+            torch.save(model.state_dict(), f.name)
+            artifact = wandb.Artifact("model", type="model")
+            artifact.add_file(f.name)
+            run.log_artifact(artifact)
+        return model
