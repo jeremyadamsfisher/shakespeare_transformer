6603302b6b9cc16526e3070f3c962ed97555adfc
jeremy <jeremy.adams.fisher@gmail.com>
2023-11-19 17:21:11 -0800

    Fix the vastai training script

--- vastai-train.sh
+++ vastai-train.sh
@@ -1,4 +1,4 @@
-# !/bin/bash
+#!/bin/env bash
 
 set -e -o pipefail
 
@@ -7,4 +7,5 @@ echo "Running in the background. You may need to edit the batch size! See train.
 nohup python -O gpt/train.py \
     model_config.batch_size=32 \
     +model_config=gpt3_small_char \
-    save_to=gs://shakespeare-gpt > train.log 2>&1 &
+    +data_config=wikipedia \
+    save_to=gs://shakespeare-gpt &> train.log &
