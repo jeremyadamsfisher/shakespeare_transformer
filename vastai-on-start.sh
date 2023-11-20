f95076eebf4626465137e6fc98673d86a63d46ee
jeremy <jeremy.adams.fisher@gmail.com>
2023-11-20 09:45:19 -0800

    Fix startup script

--- vastai-on-start.sh
+++ vastai-on-start.sh
@@ -5,19 +5,19 @@ set -e -o pipefail
 ## Set up dev environment
 
 mkdir /shakespeare
-git clone https://github.com/jeremyadamsfisher/shakespeare_transformer.git /shakespeare/shakespeare_transformer
+echo $GOOGLE_APPLICATION_CREDENTIALS_B64 \
+    | base64 --decode \
+    > /shakespeare/service_account.json
 export GOOGLE_APPLICATION_CREDENTIALS=/shakespeare/service_account.json
+git clone https://github.com/jeremyadamsfisher/shakespeare_transformer.git /shakespeare/shakespeare_transformer
 gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
+cd /shakespeare/shakespeare_transformer
 gsutil -m cp -r gs://shakespeare-gpt/char_tokenized_wikipedia_gpt3 .
 mv char_tokenized_wikipedia_gpt3 wikipedia_ds
-cd /shakespeare/shakespeare_transformer
 
 ## Add creds for later, if desired
 
 echo WANDB_API_KEY=$WANDB_API_KEY >> /etc/environment
-echo $GOOGLE_APPLICATION_CREDENTIALS_B64 \
-    | base64 --decode \
-    > /shakespeare/service_account.json
 echo GOOGLE_APPLICATION_CREDENTIALS_B64=$GOOGLE_APPLICATION_CREDENTIALS_B64 >> /etc/environment
 echo GOOGLE_APPLICATION_CREDENTIALS=/shakespeare/service_account.json >> /etc/environment
 echo PYTHONPATH=$PYTHONPATH:/shakespeare/shakespeare_transformer >> /etc/environment
